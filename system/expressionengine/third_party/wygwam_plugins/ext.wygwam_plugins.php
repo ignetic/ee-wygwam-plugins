74976baf591fd1096ead229c2c3365931302fbc9
Simon Andersohn <sy@ignetic.com>
2017-02-28 14:37:20 +0000

    Version 1.1 updates

--- system/expressionengine/third_party/wygwam_plugins/ext.wygwam_plugins.php
+++ system/expressionengine/third_party/wygwam_plugins/ext.wygwam_plugins.php
@@ -3,7 +3,7 @@
 class Wygwam_plugins_ext
 {
     var $name           = 'Wygwam Custom Plugins';
-    var $version        = '1.0';
+    var $version        = '1.1';
     var $description    = '';
     var $docs_url       = '';
     var $settings_exist = 'n';
@@ -48,7 +48,7 @@ class Wygwam_plugins_ext
         }
 
 		// Find plugins
-        $extraPlugins = $this->_include_resources();
+        $extraPlugins = $this->_include_resources($config['extraPlugins']);
 		
         // Add our plugins to CKEditor
 		if (!empty($extraPlugins))
@@ -60,25 +60,27 @@ class Wygwam_plugins_ext
         return $config;
     }
 
-    private function _include_resources()
+    private function _include_resources($plugins)
     {
+		$defaultPlugins = array('wygwam','readmore');
+		$extraPlugins = array_diff(explode(',', $plugins), $defaultPlugins);
 	
-		$extraPlugins = array();
-	
+		$loadedPlugins = array();
+
 		$plugin_url = URL_THIRD_THEMES.'wygwam_plugins/';
 		$plugin_path = PATH_THIRD_THEMES.'wygwam_plugins/';
-
-		// Scan directory for plugins
-		$plugins = array_diff(scandir($plugin_path), array('..', '.'));
 		
-		foreach ($plugins as $plugin_name)
+		// Scan directory for available plugins (activated via wygwam in extraPlugins settings)
+		
+		foreach ($extraPlugins as $plugin_name)
 		{
 			// Exclude any folders which begin with . or _
-			if (is_dir($plugin_path.$plugin_name) && (substr($plugin_name, 0, 1) != '.' && substr($plugin_name, 0, 1) != '_'))
+			if (is_file($plugin_path.$plugin_name.'/plugin.js') && (substr($plugin_name, 0, 1) != '.' && substr($plugin_name, 0, 1) != '_'))
 			{
 				// Is this the first time we've been called?
 				if ( ! isset(self::$_included_resources[$plugin_name]))
 				{
+
 					// Tell CKEditor where to find our plugin
 					ee()->cp->add_to_foot('<script type="text/javascript">CKEDITOR.plugins.addExternal("'.$plugin_name.'", "'.$plugin_url.$plugin_name.'/");</script>');
 					
@@ -86,11 +88,37 @@ class Wygwam_plugins_ext
 					self::$_included_resources[$plugin_name] = TRUE;
 				}			
 				// Add our plugin
-				$extraPlugins[] = $plugin_name;
+				$loadedPlugins[] = $plugin_name;
+			}
+		}
+		
+		// Scan directory for enabled plugins (activated by default)
+
+		if (is_dir($plugin_path))
+		{
+			$enabled_plugins = array_diff(scandir($plugin_path), array('..', '.'));
+			
+			foreach ($enabled_plugins as $plugin_name)
+			{
+				// Exclude any folders which begin with . or _
+				if (is_file($plugin_path.$plugin_name.'/plugin.js') && (substr($plugin_name, 0, 1) != '.' && substr($plugin_name, 0, 1) != '_'))
+				{
+					// Is this the first time we've been called?
+					if ( ! isset(self::$_included_resources[$plugin_name]))
+					{
+						// Tell CKEditor where to find our plugin
+						ee()->cp->add_to_foot('<script type="text/javascript">CKEDITOR.plugins.addExternal("'.$plugin_name.'", "'.$plugin_url.$plugin_name.'/");</script>');
+						
+						// Don't do that again
+						self::$_included_resources[$plugin_name] = TRUE;
+					}			
+					// Add our plugin
+					$loadedPlugins[] = $plugin_name;
+				}
 			}
 		}
 
-		return $extraPlugins;
+		return $loadedPlugins;
 
     }
 	
